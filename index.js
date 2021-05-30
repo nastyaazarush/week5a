ac8fe1111162b95d5a2c783b4576102419e4884d
nastyaazarush <aazarushkina@bk.ru>
2021-05-30 12:25:58 +0000

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-app.get('/', (req, res) =>
+app.use(bodyParser.urlencoded())
+
+const port = process.env.PORT || 8080
+
+app.get ('/', (req, res) =>
 {
-    res.render('index.ejs', {name: "Harry", lastname: "Potter"})
+    res.render('index.ejs')
 })
-const PORT = process.env.PORT || 8080
-app.listen(PORT, ()=>console.log(`listen port: ${PORT}`))
+
+app.post ('/', (req, res) =>
+{
+    const username = req.body.username
+    res.render('hello.ejs', {username: username})
+})
+
+app.listen(port, ()=> console.log (`listen port:${port}`))
